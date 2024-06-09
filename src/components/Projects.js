93c5775d8ea43b333d8ec2802057e6900204807a
reneehjk <reneehjkim11@gmail.com>
2024-06-09 13:42:39 -0400

    new changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import TodoPic from '../assets/todoList.png';
 import SonderPic from '../assets/Sonder.png';
+import calCalculator from '../assets/calCalculator.png';
 
 const Projects = () => (
   <section id="projects" style={{ padding: '2em', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
   <h2>Projects</h2>
   <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '20px' }}>
+    <div style={{ width: '400px', textAlign: 'center' }}>
+      <a href="https://github.com/reneehjk/calorie-calculator" target="_blank" rel="noopener noreferrer">
+        <img src={calCalculator} alt="Sonder." style={{ width: '100%', height: 'auto', objectFit: 'cover' }} />
+      </a>
+      <div style={{ padding: '1em', color: '#002366' }}> {/* Title div */}
+        Calorie Calculator
+      </div>
+    </div>
     <div style={{ width: '400px', textAlign: 'center' }}>
       <a href="https://github.com/reneehjk/Todo-List" target="_blank" rel="noopener noreferrer">
         <img src={TodoPic} alt="Todo List" style={{ width: '100%', height: 'auto', objectFit: 'cover' }} />
