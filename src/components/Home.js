b3bfe65af061d19c244ddc4b0d687ddc61aab7ac
reneehjk <reneehjkim11@gmail.com>
2024-06-23 12:36:37 -0400

    updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => (
       <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
         <img src={myPicture} alt="Renee" style={{ width: '300px', height: '300px', borderRadius: '50%', marginRight: '20px' }} />
         <div>
-          <h1 style={{ fontSize: '4em', margin: '0' }}>Hello I'm Renee...</h1>
+          <h1 style={{ fontSize: '100px', margin: '0' }}>Hello I'm Renee...</h1>
           <h3 style={{ fontSize: '1.5em', marginTop: '0.5em' }}>a third year computer science student at Queen's University with a focus in AI.</h3>
         </div>
       </div>
