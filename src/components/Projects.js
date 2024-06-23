dced82ee870d6fa8797893a191cca3d172b08171
reneehjk <reneehjkim11@gmail.com>
2024-06-23 12:11:03 -0400

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,11 +2,20 @@ import React from 'react';
 import TodoPic from '../assets/todoList.png';
 import SonderPic from '../assets/Sonder.png';
 import calCalculator from '../assets/calCalculator.png';
+import roomieMatch from '../assets/roomieMatch.png'
 
 const Projects = () => (
   <section id="projects" style={{ padding: '2em', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
   <h2>Projects</h2>
   <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '20px' }}>
+    <div style={{ width: '400px', textAlign: 'center' }}>
+      <a href="https://github.com/reneehjk/roomie-match" target="_blank" rel="noopener noreferrer">
+        <img src={roomieMatch} alt="RoomieMatch" style={{ width: '100%', height: 'auto', objectFit: 'cover' }} />
+      </a>
+      <div style={{ padding: '1em', color: '#002366' }}> 
+        RoomieMatch
+      </div>
+    </div>
     <div style={{ width: '400px', textAlign: 'center' }}>
       <a href="https://github.com/reneehjk/calorie-calculator" target="_blank" rel="noopener noreferrer">
         <img src={calCalculator} alt="Sonder." style={{ width: '100%', height: 'auto', objectFit: 'cover' }} />
